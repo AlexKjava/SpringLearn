ecfeffdecbe85af90ec9e47d068e7fc68bac5720
AlexKjava <a777java@rambler.ru>
2022-02-10 21:54:56 +0300

    final

--- src/com/kciray/ProductService.java
+++ src/com/kciray/ProductService.java
@@ -1,9 +1,15 @@
 package com.kciray;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.stereotype.Component;
 
+import javax.annotation.PreDestroy;
+
 @Component
-public class ProductService {
+public class ProductService implements InitializingBean, DisposableBean {
+    @Autowired
     private PromotionsService promotionsService;
 
     public PromotionsService getPromotionsService() {
@@ -12,5 +18,20 @@ public class ProductService {
 
     public void setPromotionsService(PromotionsService promotionsService) {
         this.promotionsService = promotionsService;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        System.out.println("PromotionsService init...");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("PromotionsService destroy...");
+    }
+
+    @PreDestroy
+    public void destroy2() {
+        System.out.println("PromotionsService @PreDestroy...");
     }
 }
